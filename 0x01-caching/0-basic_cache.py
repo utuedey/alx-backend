44f04bdeb042afc717db1e3a367db8548504af0b
utuedey <utuedeyejosph@gmail.com>
2023-07-28 21:28:24 +0000

    add return statement

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -15,6 +15,8 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item in the cache"""
+        if key is None or item is None:
+            return
         self.cache_data[key] = item
 
     def get(self, key):
