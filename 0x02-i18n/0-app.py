92ca9fb5852fe1ea133096e077a2d739992a7e3c
utuedey <utuedeyejosph@gmail.com>
2023-08-05 16:08:30 +0000

    modfiy 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """Home page of the app"""
-    return render_template('templates/0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
