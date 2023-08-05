f4d6556b0ae5b6a8a3f4b4042a27998375785f01
utuedey <utuedeyejosph@gmail.com>
2023-08-05 16:22:44 +0000

    import render_template

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,16 +3,17 @@
 0-app module - A Basic flask app
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route("/")
-def index():
+def index() -> str:
     """Home page of the app"""
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
