6aad4e9fbddafb1631cbbb9feed98319edde0ca9
utuedey <utuedeyejosph@gmail.com>
2023-08-30 08:42:42 +0000

    create 6-job_creator.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -7,9 +7,9 @@ const jobData = {
 }
 
 const job = queue.create('push_notification_code', jobData)
-              .save((error) => {
-                if (!error) console.log(`Notification job created: ${job.id}`);
-              });
+             .save((error) => {
+               if (!error) console.log(`Notification job created: ${job.id}`);
+             });
 
 job.on('complete', () => console.log('Notification job completed'));
 job.on('failed', () => console.log('Notification job failed'));
