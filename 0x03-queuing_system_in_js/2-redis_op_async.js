fef22fb6dc3ec44daff01807f058949f75e18517
utuedey <utuedeyejosph@gmail.com>
2023-08-30 08:36:31 +0000

    create 2-redis_op_async.js

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,29 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+const promisifiedGet = promisify(client.get).bind(client);
+
+client.on("error", (error) => {
+  if (error) {
+    console.log(`Redis client not connected to the server: ${error}`)};
+}).on('ready', () => {
+    console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (error, reply) => {
+     redis.print(`Reply: ${reply}`);
+  });
+}
+
+const displaySchoolValue = async (schoolName) => {
+    const reply = await promisifiedGet(schoolName);
+    console.log(reply);
+}
+
+(async() => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+    await displaySchoolValue('HolbertonSanFrancisco');
+})();
